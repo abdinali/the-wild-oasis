e4f02a481c9be2166fad79856c5dbbc264018709
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-04-27 13:15:41 -0400

    feat: add a form to create cabins with react hook form

--- src/services/apiCabins.js
+++ src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supabase';
+import supabase, { supabaseUrl } from './supabase';
 
 export async function getCabins() {
 	const { data: cabins, error } = await supabase.from('cabins').select('*');
@@ -8,6 +8,36 @@ export async function getCabins() {
 	}
 
 	return cabins;
+}
+
+export async function createCabin(newCabin) {
+	const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll('/', '');
+	const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+	console.log(imagePath);
+	// 1. Create the cabin
+	const { data, error } = await supabase
+		.from('cabins')
+		.insert([{ ...newCabin, image: imagePath }])
+		.select();
+
+	if (error) {
+		console.error(error);
+		throw new Error('Cabin could not be created.');
+	}
+
+	// 2. Upload image
+	const { error: storageError } = await supabase.storage
+		.from('cabin-images')
+		.upload(imageName, newCabin.image);
+
+	// 3. Delete cabin IF there was an error uploading image
+	if (storageError) {
+		await supabase.from('cabins').delete().eq('id', data.id);
+		console.error(storageError);
+		throw new Error('Cabin image could not be uploaded. Consequently, the cabin was not created.');
+	}
+
+	return data;
 }
 
 export async function deleteCabin(id) {
