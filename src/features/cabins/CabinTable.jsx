f91eb35f6ca04a9b1580f2fe06ffe1ec9d5ca98c
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-05-16 13:39:25 -0400

    feat: create reusable filter component and apply it to cabins page

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from 'react-router-dom';
 import { useGetCabins } from './useGetCabins';
 import Spinner from '../../ui/Spinner';
 import Table from '../../ui/Table';
@@ -6,7 +7,20 @@ import Menus from '../../ui/Menus';
 
 function CabinTable() {
 	const { isLoading, cabins } = useGetCabins();
+	const [searchParams] = useSearchParams();
+
+	const filterValue = searchParams.get('discount') || 'all';
+
+	let filteredCabins;
+
+	if (filterValue === 'all') filteredCabins = cabins;
+	if (filterValue === 'no-discount')
+		filteredCabins = cabins?.filter((cabin) => cabin.discount === 0);
+	if (filterValue === 'with-discount')
+		filteredCabins = cabins?.filter((cabin) => cabin.discount > 0);
+
 	if (isLoading) return <Spinner />;
+
 	return (
 		<Menus>
 			<Table columns="0.6fr 1.8fr 2.2fr 1fr 1fr 1fr">
@@ -18,7 +32,10 @@ function CabinTable() {
 					<div>Discount</div>
 					<div></div>
 				</Table.Header>
-				<Table.Body data={cabins} render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />} />
+				<Table.Body
+					data={filteredCabins}
+					render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
+				/>
 			</Table>
 		</Menus>
 	);
