af6de21204b9d89d5e6e26ef4693224b6bda64de
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-05-23 12:09:49 -0400

    feat: implement api-side filter and sort capabilities to bookings page

--- src/features/bookings/useGetBookings.js
+++ src/features/bookings/useGetBookings.js
@@ -1,14 +1,27 @@
 import { useQuery } from '@tanstack/react-query';
 import { getBookings } from '../../services/apiBookings';
+import { useSearchParams } from 'react-router-dom';
 
 export function useGetBookings() {
+	const [searchParams] = useSearchParams();
+
+	// 1. FILTER
+	const filterValue = searchParams.get('status') || 'all';
+	const filter =
+		!filterValue || filterValue === 'all' ? null : { field: 'status', value: filterValue };
+
+	// 2. SORT
+	const sortByRaw = searchParams.get('sortBy') || 'startDate-desc';
+	const [field, direction] = sortByRaw.split('-');
+	const sortBy = { field, direction };
+
 	const {
 		isLoading,
 		error,
 		data: bookings,
 	} = useQuery({
-		queryKey: ['bookings'],
-		queryFn: getBookings,
+		queryKey: ['bookings', { filter }, { sortBy }],
+		queryFn: () => getBookings({ filter, sortBy }),
 	});
 
 	return { isLoading, error, bookings };
