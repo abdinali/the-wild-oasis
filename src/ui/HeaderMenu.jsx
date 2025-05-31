7e67cfd17bdaf7784666ad44ed6703edc53fafc3
Abdinasir Ali <131626624+abdinali@users.noreply.github.com>
2025-05-31 13:29:48 -0400

    feat: add a dark mode feature with a toggle component using a context provider

--- src/ui/HeaderMenu.jsx
+++ src/ui/HeaderMenu.jsx
@@ -1,8 +1,10 @@
 import { useNavigate } from 'react-router-dom';
+import { HiOutlineUser } from 'react-icons/hi2';
 import styled from 'styled-components';
+
 import Logout from '../features/authentication/Logout';
 import ButtonIcon from './ButtonIcon';
-import { HiOutlineUser } from 'react-icons/hi2';
+import DarkModeToggle from './DarkModeToggle';
 
 const StyledHeaderMenu = styled.ul`
 	display: flex;
@@ -18,6 +20,9 @@ function HeaderMenu() {
 				<ButtonIcon onClick={() => navigate('/account')}>
 					<HiOutlineUser />
 				</ButtonIcon>
+			</li>
+			<li>
+				<DarkModeToggle />
 			</li>
 			<li>
 				<Logout />
